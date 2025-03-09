cd6314f2106d72b3befd8c9d20e9707befb88690
jhdark <darkj385@mit.edu>
2025-03-09 09:16:50 -0400

    example vtk reader file

---
+++ src/vtk2dolfinx/vtk_reader.py
@@ -0,0 +1,23 @@
+import dolfinx
+import pyvista
+
+class VTKReader:
+    """"""
+
+    def __init__(self):
+        pass
+
+
+class StructuredGridReader(VTKReader):
+    """"""
+
+    def __init__(self):
+        pass
+
+
+
+class UnstructuredMeshReader(VTKReader):
+    """"""
+
+    def __init__(self):
+        pass

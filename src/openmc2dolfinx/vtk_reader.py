c22ae1ea0e2dd2efea43c7de3c9c2495e153730f
jhdark <darkj385@mit.edu>
2025-03-10 21:02:21 -0400

    create parent/child classes, type hinting, docs

--- src/openmc2dolfinx/vtk_reader.py
+++ src/openmc2dolfinx/vtk_reader.py
@@ -2,26 +2,37 @@ from mpi4py import MPI
 
 import basix
 import dolfinx
+import numpy as np
 import pyvista
+import pyvista.core.pointset
 import ufl
 from dolfinx.mesh import create_mesh
 
 __all__ = ["StructuredGridReader", "UnstructuredMeshReader"]
 
 
-class UnstructuredMeshReader:
+class OpenMC2Dolfinx:
     """
-    Reads an OpenMC .vtk results file with an unstructured mesh and converts the
-    velocity data into a dolfinx.fem.Function
+    Base OpenMC2Dolfinx Mesh Reader
 
-        Args:
-            filename: the filename
+    Reads an OpenMC .vtk results file mesh and converts the data into a
+    dolfinx.fem.Function
 
-        Attributes:
-            filename: the filename
-            grid: the mesh and results from the OpenMC .vtk file
-            connectivity: The OpenMC mesh cell connectivity
+    Args:
+        filename: the filename
+
+    Attributes:
+        filename: the filename
+        grid: the mesh and results from the OpenMC .vtk file
+        connectivity: The OpenMC mesh cell connectivity
+        dolfinx_mesh: the dolfinx mesh
     """
+
+    filename: str
+
+    grid: pyvista.core.pointset.UnstructuredGrid | pyvista.core.pointset.StructuredGrid
+    connectivity: np.ndarray
+    dolfinx_mesh: dolfinx.mesh.Mesh
 
     def __init__(self, filename):
         self.filename = filename
@@ -36,10 +47,20 @@ class UnstructuredMeshReader:
 
         self.grid = pyvista.read(self.filename)
 
-        # Extract connectivity information
-        self.cell_connectivity = self.grid.cells_dict[10]
+    def create_dolfinx_mesh(self, cell_type="tetrahedron"):
+        degree = 1  # Set polynomial degree
+        cell = ufl.Cell(f"{cell_type}")
+        mesh_element = basix.ufl.element(
+            "Lagrange", cell.cellname(), degree, shape=(3,)
+        )
 
-    def create_dolfinx_fucntion(self, data: str = "mean"):
+        # Create dolfinx Mesh
+        mesh_ufl = ufl.Mesh(mesh_element)
+        self.dolfinx_mesh = create_mesh(
+            MPI.COMM_WORLD, self.cell_connectivity, self.grid.points, mesh_ufl
+        )
+
+    def create_dolfinx_function(self, data: str = "mean"):
         """reads the filename of the OpenMC file
 
         args:
@@ -49,18 +70,7 @@ class UnstructuredMeshReader:
             dolfinx function with openmc results mapped
         """
 
-        degree = 1  # Set polynomial degree
-        cell = ufl.Cell("tetrahedron")
-        mesh_element = basix.ufl.element(
-            "Lagrange", cell.cellname(), degree, shape=(3,)
-        )
-
-        # Create dolfinx Mesh
-        mesh_ufl = ufl.Mesh(mesh_element)
-        dolfinx_mesh = create_mesh(
-            MPI.COMM_WORLD, self.cell_connectivity, self.grid.points, mesh_ufl
-        )
-        function_space = dolfinx.fem.functionspace(dolfinx_mesh, ("DG", 0))
+        function_space = dolfinx.fem.functionspace(self.dolfinx_mesh, ("DG", 0))
         u = dolfinx.fem.Function(function_space)
 
         u.x.array[:] = self.grid.cell_data[f"{data}"][
@@ -70,7 +80,43 @@ class UnstructuredMeshReader:
         return u
 
 
-class StructuredGridReader:
+class UnstructuredMeshReader(OpenMC2Dolfinx):
+    """
+    Unstructured Mesh Reader
+
+    Reads an OpenMC .vtk results file with unstructured meshes and converts the data
+    into a dolfinx.fem.Function
+
+    Args:
+        filename: the filename
+
+    Attributes:
+        filename: the filename
+        grid: the mesh and results from the OpenMC .vtk file
+        connectivity: The OpenMC mesh cell connectivity
+        dolfinx_mesh: the dolfinx mesh
+    """
+
+    filename: str
+
+    grid: pyvista.core.pointset.UnstructuredGrid
+    connectivity: np.ndarray
+    dolfinx_mesh: dolfinx.mesh.Mesh
+
+    def read_vtk_file(self):
+        """reads the filename of the OpenMC file, extracts the data, creates the cell
+        connectivity between the openmc mesh and the dolfinx mesh and finally creates
+        the dolfinx mesh"""
+
+        self.grid = pyvista.read(self.filename)
+
+        # Extract connectivity information
+        self.cell_connectivity = self.grid.cells_dict[10]
+
+        self.create_dolfinx_mesh(cell_type="tetrahedron")
+
+
+class StructuredGridReader(OpenMC2Dolfinx):
     """
     Reads an OpenMC .vtk results file with an structured mesh and converts the
     velocity data into a dolfinx.fem.Function
@@ -84,16 +130,16 @@ class StructuredGridReader:
             connectivity: The OpenMC mesh cell connectivity
     """
 
-    def __init__(self, filename):
-        self.filename = filename
+    filename: str
 
-        self.grid = None
-        self.cell_connectivity = []
-
-        self.read_vtk_file()
+    grid: pyvista.core.pointset.StructuredGrid
+    connectivity: np.ndarray
+    dolfinx_mesh: dolfinx.mesh.Mesh
 
     def read_vtk_file(self):
-        """reads the filename of the OpenMC file"""
+        """reads the filename of the OpenMC file, extracts the data, creates the cell
+        connectivity between the openmc mesh and the dolfinx mesh and finally creates
+        the dolfinx mesh"""
 
         self.grid = pyvista.read(self.filename)
 
@@ -102,38 +148,12 @@ class StructuredGridReader:
         # Extract connectivity information
         ordering = [0, 1, 3, 2, 4, 5, 7, 6]
 
+        self.cell_connectivity = []
+
         # Extract all cell connectivity data at once
         for i in range(num_cells):
             cell = self.grid.GetCell(i)  # Get the i-th cell
             point_ids = [cell.GetPointId(j) for j in ordering]  # Extract connectivity
             self.cell_connectivity.append(point_ids)
 
-    def create_dolfinx_function(self, data: str = "mean"):
-        """reads the filename of the OpenMC file
-
-        args:
-            data: the name of the data to extract from the vtk file
-
-        returns:
-            dolfinx function with openmc results mapped
-        """
-        degree = 1  # Set polynomial degree
-        cell = ufl.Cell("hexahedron")
-        mesh_element = basix.ufl.element(
-            "Lagrange", cell.cellname(), degree, shape=(3,)
-        )
-
-        # Create dolfinx Mesh
-        mesh_ufl = ufl.Mesh(mesh_element)
-        self.dolfinx_mesh = create_mesh(
-            MPI.COMM_WORLD, self.cell_connectivity, self.grid.points, mesh_ufl
-        )
-
-        function_space = dolfinx.fem.functionspace(self.dolfinx_mesh, ("DG", 0))
-        u = dolfinx.fem.Function(function_space)
-
-        u.x.array[:] = self.grid.cell_data[f"{data}"][
-            self.dolfinx_mesh.topology.original_cell_index
-        ]
-
-        return u
+        self.create_dolfinx_mesh(cell_type="hexahedron")
